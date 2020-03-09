2b3c2184654166b4ecfe22da0600a0104eb4e974
Guru-Sandhu <arsh3874@gmail.com>
2020-03-09 11:34:08 -0700

    install morgan middleware

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const logger = require('morgan')
 
 const app = express();
+
+app.use(logger('dev'))
 
 app.get('/', (req,res) => {
   res.send('<h1>Hello World</h1>')
