7ae3fda646c6a3db41bf2afe704cab58fc655e65
Guru-Sandhu <arsh3874@gmail.com>
2020-03-09 10:52:47 -0700

    add get / route

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express')
 
 const app = express();
 
+app.get('/', (req,res) => {
+  res.send('Hello World')
+})
+
 const PORT = 4000;
 const DOMAIN = 'localhost';
-
 app.listen(PORT,DOMAIN, () => {
   console.log(`Server Listening on ${DOMAIN} ${PORT}`)
 })
