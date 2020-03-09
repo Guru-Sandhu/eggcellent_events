890a1c705b69ffb6c591189dfba7693b09bbb521
Guru-Sandhu <arsh3874@gmail.com>
2020-03-09 10:58:26 -0700

    add /survey route

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const express = require('express')
 const app = express();
 
 app.get('/', (req,res) => {
-  res.send('Hello World')
+  res.send('<h1>Hello World</h1>')
+})
+
+app.get('/survey',(req,res) => {
+  res.send('Hello')
 })
 
 const PORT = 4000;
