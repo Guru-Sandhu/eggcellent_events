a7da9964f12b0da55dc63c4f579d6121d7624b82
Guru-Sandhu <arsh3874@gmail.com>
2020-03-10 09:36:43 -0700

    set up knex file

--- knexfile.js
+++ knexfile.js
@@ -3,42 +3,15 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: 'pg',
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      database:'eggcellent_events_wadd',
+      username:'guru',
+      password:'Sandhu@123'
     },
     migrations: {
+      directory: './db/migrations',
       tableName: 'knex_migrations'
     }
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
