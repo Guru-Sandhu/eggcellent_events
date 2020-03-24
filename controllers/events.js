bc7877690fcd56ad2e1978d61beb6d2d12c553b1
Guru-Sandhu <arsh3874@gmail.com>
2020-03-24 14:36:12 -0700

    refactor events controller for edit and update events

--- controllers/events.js
+++ controllers/events.js
@@ -41,21 +41,19 @@ module.exports = {
   },
   edit: (req, res) => {
     const { id } = req.params
-    event.one(id)
+    new event({ id }).fetch()
       .then(event => {
+        event = event.toJSON()
           res.render('events/edit', { event })
       })
   },
   update: (req,res) => {
     const { id } = req.params
     const { title, description } = req.body
-    event.update({ id, title, description })
+    new event({ id}).save({ title, description })
       .then(event => {
-        if (event) {
           res.redirect(`/events/${event.id}`)
-        } else {
-          res.redirect(`/events/${id}/edit`)
-        }
       })
+      .catch(err => console.log(err))
   }
 }
