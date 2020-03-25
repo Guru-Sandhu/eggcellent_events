ab1853955e7438fc6a3ab6ed7bec1772e2693586
Guru-Sandhu <arsh3874@gmail.com>
2020-03-25 11:19:28 -0700

    added routes for users

---
+++ routes/users.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+const { users } = require('../controllers')
+
+router.get('/', users.index)
+
+router.get('/new', users.new)
+
+router.get('/:id', users.show)
+
+router.get('/:id/edit', users.edit)
+
+router.patch('/:id', users.update)
+
+router.post('/new',users.create)
+
+router.delete('/:id', users.delete)
+
+module.exports = router
